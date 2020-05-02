6ce98f5750586ed8bd7e65c374cddd164fb5af5a
adz0612 <54070532+adz0612@users.noreply.github.com>
2020-05-02 10:43:04 +0530

    Update script.py

--- py/script.py
+++ py/script.py
@@ -1,19 +1,4 @@
 import cv2
-fc= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-
-img = cv2.imread('test2.jpg')
-
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-faces = fc.detectMultiScale(gray,1.1,4)
-for (x,y,w,h) in faces:
-    cv2.rectangle(img , (x,y) , (x+w ,y+h),(255,0,0),6)
-cv2.imshow('img',img)
-cv2.waitKey()
-
-
-import cv2
 import face_recognition
 
 # Get a reference to webcam
