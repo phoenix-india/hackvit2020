0b6f8d7ed0ed09a6e498a82d5ae271ada94a8c4e
parvg555 <61341517+parvg555@users.noreply.github.com>
2020-05-02 10:26:20 +0530

    Add files via upload

---
+++ py/script.py
@@ -0,0 +1,59 @@
+import cv2
+fc= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+img = cv2.imread('test2.jpg')
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = fc.detectMultiScale(gray,1.1,4)
+for (x,y,w,h) in faces:
+    cv2.rectangle(img , (x,y) , (x+w ,y+h),(255,0,0),6)
+cv2.imshow('img',img)
+cv2.waitKey()
+
+
+import cv2
+import face_recognition
+
+# Get a reference to webcam
+video_capture = cv2.VideoCapture(0)
+
+# Initialize variables
+face_locations = []
+def detectFace():
+    while True:
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_frame = frame[:, :, ::-1]
+
+        # Find all the faces in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_frame)
+        numOfFaces = len(face_locations)
+
+        # Display the results
+        for top, right, bottom, left in face_locations:
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        if(numOfFaces>1 or numOfFaces<1):
+
+            return 0
+
+
+
+
+
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+aa = detectFace()
