9fb0aac444c79af56d1e4891d229f5e0b84512d4
parvg555 <61341517+parvg555@users.noreply.github.com>
2020-05-02 10:06:24 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,104 @@
+<style type="text/css">
+<!--
+body {
+  margin-left: 0px;
+  margin-top: 0px;
+}
+-->
+</style>
+<link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+<script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script type="text/javascript" src="jquery.timer.js"></script>
+    <script type="text/javascript" src="res/demo.js"></script>
+<script type="text/javascript" src="js/visibility.js?n=1"></script>
+<script type="text/javascript" src="js/check.js?n=1"></script>
+<script type="text/javascript" src="js/timer.js?n=1"></script>
+<!--<table border="0" width="100%" cellspacing="0" cellpadding="0" background="image/click.png">
+  <tr>
+    <td width="90%" valign="top">
+You can modify the text, color, size, number of loops and more on the flash header by editing the text file (fence.txt) included in the zip file.-->
+<!--<div align="left"><object classid=clsid:D27CDB6E-AE6D-11cf-96B8-444553540000
+codebase=http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,2,0
+width=500
+height=68>
+<param name=movie value=image/fence.swf>
+<param name=quality value=high>
+<param name=BGCOLOR value=#000000>
+<param name=SCALE value=showall>
+<param name=wmode value=transparent> 
+<embed src=image/fence.swf
+quality=high
+pluginspage=http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash type=application/x-shockwave-flash
+width=500
+height=68
+bgcolor=#000000
+scale= showall>
+</embed>
+</object></div></td>-->
+    <td width="10%">
+     <img border="0" src="image/h.jpg" width="100%" height="150" align="right"></td>
+  </tr>
+</table>
+<!--<table border="0" width="100%" cellspacing="0" cellpadding="0" bgcolor="#000000" background="img/blackbar.jpg">
+  <tr>
+    <td width="100%" align="right"><img border="0" src="image/blackbar.jpg" width="89" height="15"></td>
+  </tr>
+  </Table>-->
+  <Table width="100%">
+  <tr>
+  <td>
+  <?php @$_SESSION['login']; 
+  error_reporting(1);
+  ?>
+  </td>
+    <td>
+<?php
+      echo "<span id=\"countdown\">00:10:00</span>";
+
+  
+  if(isset($_SESSION['login']))
+  {
+   echo "<div align=\"right\"><strong><a href=\"index.php\"> Home </a>|<a href=\"signout.php\">Signout</a></strong></div>";
+   }
+   else
+   {
+    echo "&nbsp;";
+   }
+  ?>
+  </td>
+  
+  </tr>
+  
+</table>
+
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+<?php
+
+$command = escapeshellcmd('py/script.py');
+$output = shell_exec($command);
+echo $output;
+?>
+
+<!--document.onkeydown = function (e) {
+       // return false;
+//-->
+
+
+
+
